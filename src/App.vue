c753e7db2aaf7a50bfac82b0e972d4afea07cd70
Jonas <jonas@sturmente.de>
2025-02-13 23:00:59 +0100

    Started designing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+	<div id="content">
+		<div id="title">Finance Calc</div>
+	</div>
+</template>
 
-<style scoped></style>
+<style scoped>
+#content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: flex-start;
+	widows: 100vw;
+	height: 100vh;
+
+	padding: 20px;
+	box-sizing: border-box;
+}
+</style>
+
+<style>
+#title {
+	font-size: 2.5rem;
+	color: var(--accent-color);
+}
+</style>
